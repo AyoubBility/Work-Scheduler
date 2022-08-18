47010c36687eb8a7fd6f3ee1c66c78350475e4d6
Ayoub <biliad8786@gmail.com>
2022-08-18 01:31:47 -0400

    Second commit

--- script.js
+++ script.js
@@ -1,49 +1,61 @@
-$(document).ready(function () {// tells engine to load 1)html & 2)css first.
+var hour1 = $("#hour1 .description")
+var hour2 = $("#hour2 .description")
+var hour3 = $("#hour3 .description")
+var hour4 = $("#hour4 .description")
+var hour5 = $("#hour5 .description")
+
+var hour8 = $("#hour8 .description")
+var hour9 = $("#hour9 .description")
+var hour10 = $("#hour10 .description")
+var hour11 = $("#hour11 .description")
+var hour12 = $("#hour12 .description")
+
+$(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     $(".saveBtn").on("click", function () {
-        console.log("You saved. Try refreshing.")
-        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
-        
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        console.log("You saved", text)
+
         localStorage.setItem(time, text);
-        console.log(text,"was added to local storage")
+        if(text === ""){
+            console.log("Textbox is empty.")
+        } else {
+            console.log(text,"was added to local storage")
+        }
     })
-    //load any saved data from LocalStorage - do this for each hour created. Should follow html 24 hour to 12 hour conversion.
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    
+    //moring
+    $(hour8).val(localStorage.getItem("hour8"));
+    $(hour9).val(localStorage.getItem("hour9"));
+    $(hour10).val(localStorage.getItem("hour10"));
+    $(hour11).val(localStorage.getItem("hour11"));
+    $(hour12).val(localStorage.getItem("hour12"));
+
+    //afternoon
+    $(hour1).val(localStorage.getItem("hour1"));
+    $(hour2).val(localStorage.getItem("hour2"));
+    $(hour3).val(localStorage.getItem("hour3"));
+    $(hour4).val(localStorage.getItem("hour4"));
+    $(hour5).val(localStorage.getItem("hour5"));
 
     function hourTracker() {
         var currentHour = moment().hour();
-
-        // loop over time blocks
         $(".time-block").each(function () {
             var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-
-            //check if we've moved past this time, click into css/html given classes of past, present, or future
             if (blockHour < currentHour) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
                 $(this).removeClass("present");
-            }
-            else if (blockHour === currentHour) {
+            }else if (blockHour === currentHour) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
                 $(this).removeClass("future");
-            }
-            else {
+            }else {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
-            }
-        })
+            }})
     }
     hourTracker();
 })
